b46e84a1ccedfbe16687deb7853bc8fb52e19d52
Leonardo Santos Azevedo <leonardosantosazevedo195@gmail.com>
2024-03-16 18:58:36 -0300

    git inicial

---
+++ src/components/Perfil/Formulario/index.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react"
+
+const Formulario = () => {
+    const [materiaA, setMateriaA] = useState(0);
+    const [materiaB, setMateriaB] = useState(0);
+    const [materiaC, setMateriaC] = useState(0);
+    const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        console.log("O componente iniciou");
+
+        return () => {
+            console.log("O componente finalizou");
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log("O estado nome mudou");
+    }, [nome]);
+
+    useEffect(() => {
+        console.log("Materia A mudou para: " + materiaA);
+    }, [materiaA, materiaB, materiaC]);
+
+    const alteraNome = (evento) => {
+        setNome(estadoAnterior => {
+            console.log(estadoAnterior);
+            return evento.target.value;
+        })
+    }
+
+    const renderizaResultado = () => {
+        const soma = materiaA + materiaB + materiaC;
+        const media = soma / 3;
+
+
+
+        if (media >= 7) {
+            return (
+                <p>Olá {nome}, você foi aprovado</p>
+            )
+        } else {
+            return (
+                <p>Olá {nome}, você foi reprovado</p>
+            )
+        }
+    }
+
+    return (
+        <form>
+            <input type="text" placeholder="Seu nome" onChange={alteraNome} />
+            <input type="number" placeholder="Nota matéria A" onChange={({ target }) => setMateriaA(parseInt(target.value))} />
+            <input type="number" placeholder="Nota matéria B" onChange={evento => setMateriaB(parseInt(evento.target.value))} />
+            <input type="number" placeholder="Nota matéria C" onChange={evento => setMateriaC(parseInt(evento.target.value))} />
+            {renderizaResultado()}
+        </form>
+    )
+}
+
+export default Formulario 
